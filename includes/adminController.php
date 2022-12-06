bd797d78af2ce470447c9a52faae24a6f530b7f7
abongilemanci <37101442+abongilemanci@users.noreply.github.com>
2022-12-06 17:19:06 +0200

    Update adminController.php

--- includes/adminController.php
+++ includes/adminController.php
@@ -5,47 +5,71 @@ session_start();
 //Include database connection and functions scripts
 include_once "./../scripts/db.php";
 
-if(isset($_POST["getUser"])){
+if (isset($_POST["getUser"])) {
     $query = "SELECT * FROM `users`";
     $result = mysqli_query($con, $query);
+
+    $out = '<tbody>';
     if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        $out ='';
-        // while ($row = mysqli_fetch_array($result))
-        // {
-        //     $out .= '<tr class="">
-        //     <td scope="row">'.$row['username'].'</td>
-        //     <td>'.$row['email'].'</td>
-        //     <td>'.$row['user_type'].'</td>
-        //     <td>'.$row['user_type'].'</td>
-        // </tr>';
-        // }
-        echo "<tr><td>User </td></tr><br/><tr><td></td></tr><br/><tr><td>List</td></tr>";
-        // echo $row['email'];
-        // echo '<tr class="">
-        //         <td scope="row">'.$row['username'].'</td>
-        //         <td>'.$row['email'].'</td>
-        //         <td>'.$row['user_type'].'</td>
-        //         <td>'.$row['user_type'].'</td>
-        //     </tr>';
+
+        while ($row = mysqli_fetch_array($result)) {
+            $out .= '<tr class="">
+                <td scope="row">' . $row['username'] . '</td>
+                <td>' . $row['email'] . '</td>
+                <td>' . $row['user_type'] . '</td>
+                <td>
+                    <a class="btn btn-danger btn-sm btnDelete" type="button" href="#" data-id="' . $row["id"] . '">
+                        <i class="fa-solid fa-trash fa-fw"></i> DELETE
+                    </a>
+                </td>
+            </tr>';
+        }
+    } else {
+        echo "<tr><td>No record Found int the database.</td></tr>";
     }
-    else {
-    echo 0;
+
+    $out .= '</tbody>';
+    echo $out;
 }
 
-}
-
-if(isset($_POST["searchUser"])){
+if (isset($_POST["searchUser"])) {
     $key = $_POST['key'];
-    $query = "SELECT * FROM `users` WHERE `email` =".$key;
+    $query = "SELECT * FROM `users` WHERE `email` LIKE '%".$_POST['key']."%'";
     $result = mysqli_query($con, $query);
+
+    $out = '<tbody>';
     if (mysqli_num_rows($result) > 0) {
-        $row = mysqli_fetch_assoc($result);
-        echo $row['email'];
-    }
-    else{
-        echo 0;
+
+        while ($row = mysqli_fetch_array($result)) {
+            $out .= '<tr class="">
+                <td scope="row">' . $row['username'] . '</td>
+                <td>' . $row['email'] . '</td>
+                <td>' . $row['user_type'] . '</td>
+                <td>
+                    <a class="btn btn-danger btn-sm" type="button" href="#" data-id="' . $row["user_type"] . '">
+                        <i class="fa-solid fa-trash fa-fw"></i> DELETE
+                    </a>
+                </td>
+            </tr>';
+        }
+    } else {
+        echo "<tr><td>No record matches <kbd>".$_POST['key']."</kbd> Found int the database.</td></tr>";
     }
 
+    $out .= '</tbody>';
+
+    echo $out;
 }
-?>
+
+if (isset($_POST["deleteUser"])) {
+    $query = "DELETE FROM `users` WHERE `users`.`id` = ".$_POST['id'];
+    $result = mysqli_query($con, $query);
+
+    if ($result = mysqli_query($con, $query)) {
+        echo 'User information was successfully removed from database';
+    } else {
+        echo "Oooops!, Something went wrong while removing";
+    }
+
+
+}
